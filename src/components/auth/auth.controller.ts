516324a85150a6b7ea729a6e11898a88d551e4b8
Carlos DIaz <c.diazadriann@gmail.com>
2025-02-05 17:52:58 -0400

    agregando dto

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 
 //Propio
 import { NAME_NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,8 +13,8 @@ export class AuthController {
   ) {}
 
   @Post('register')
-  private registerUser() {
-    return this.client.send('auth.register.user', {}).pipe(
+  private registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
       catchError((err) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         throw new RpcException(err);
@@ -21,9 +22,9 @@ export class AuthController {
     );
   }
 
-  @Post('auth.login.user')
-  private loginUser() {
-    return this.client.send('auth.register.user', {}).pipe(
+  @Post('login')
+  private loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
       catchError((err) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         throw new RpcException(err);
@@ -31,9 +32,9 @@ export class AuthController {
     );
   }
 
-  @Post('auth.verify.token')
+  @Post('verify-token')
   private verifyToken() {
-    return this.client.send('auth.register.user', {}).pipe(
+    return this.client.send('auth.verify.token', {}).pipe(
       catchError((err) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         throw new RpcException(err);
